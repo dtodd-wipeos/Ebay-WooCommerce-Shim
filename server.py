baa95bc03cd09582fd70423fa2d76e670198fd92
David Todd <dtodd@oceantech.com>
2020-02-04 15:51:51 -0600

    consistency

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from pprint import pprint
 from optparse import OptionParser
 from shim.ebay import EbayShim
+from shim.woo import WooCommerceShim
 
 def parse_options():
     parser = OptionParser(usage="Usage: %prog [options]")
@@ -22,7 +23,10 @@ if __name__ == '__main__':
     # Currently unused
     (opts, args) = parse_options()
 
-    shim = EbayShim()
-    shim.set_date_range(start_date='2020-02-01', days=1).set_range_filter()
+    ebay_shim = EbayShim()
+    woo_shim = WooCommerceShim()
 
-    shim.try_command('get_seller_list')
+    # Store all items that started within this date range
+    ebay_shim.set_date_range(start_date='2020-02-01', days=1).set_range_filter()
+    # This will also store metadata for items that are still active
+    ebay_shim.try_command('get_seller_list')
