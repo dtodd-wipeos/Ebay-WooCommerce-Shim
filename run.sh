2dc385bd3c978f105b8d4f7656bc415f7855b879
David Todd <dtodd@oceantech.com>
2020-02-06 17:24:05 -0600

    fix typo

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Copyright 2020 David Todd <dtodd@oceantech.com>
 # License: Properitary
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 1 ]]; then
     source bin/run.sh $1 1
 else
     source bin/run.sh production 1
