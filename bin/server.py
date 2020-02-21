7c82e6515f04afb8b6117289bd984390df4e0f56
David Todd <dtodd@oceantech.com>
2020-02-21 15:27:24 -0600

    Reimplement without threading or queue class

--- bin/server.py
+++ bin/server.py
@@ -6,85 +6,103 @@
 import os
 import sys
 import logging
-import threading
 
-from shim.queue import (
-    EbayDownloadQueue,
-    ProductUploadQueue,
-    ProductDeletionQueue,
-    ProductImageQueue
-)
+from shim.ebay import EbayShim
+from shim.woo import WooCommerceShim
 from shim.util import LOG_HANDLER
 
 class Server:
     """
-        Provides a framework for the threading of the server
+        Provides a framework for sequentially running the shim commands
     """
 
     def __init__(self):
         """
-            Sets up internals, such as logging and a container
-            for any threads that are running
+            Sets up internals, such as logging and the API Shims
         """
         # Set up logging
         self.log = logging.getLogger(__name__)
         self.log.setLevel(os.environ.get('log_level', 'INFO'))
         self.log.addHandler(LOG_HANDLER)
 
-        self.threads = []
-        self.start()
+        # Shims
+        self.ebay = EbayShim()
+        self.woo = WooCommerceShim()
 
-    def __start_threads(self):
+        self.active_item_ids = self.woo.db_get_active_item_ids()
+        self.inactive_item_ids = self.woo.db_get_inactive_uploaded_item_ids()
+
+    def __ebay_download_products(self):
         """
-            Starts a server thread for each "Queue" class
+            Sets up a date filter to get all items that will
+            be automatically ended at the end of the range and
+            gets the products from that
         """
-        self.log.info('Starting background threads')
+        self.log.info('Setting date range filter to today and ending at 35 days in the future')
+        self.ebay.set_date_range(start_date='', days=35, range_type='End').set_range_filter()
+        self.ebay.try_command('get_seller_list')
 
-        ebay_queue = threading.Thread(target=EbayDownloadQueue)
-        self.threads.append(ebay_queue)
-
-        product_upload_queue = threading.Thread(target=ProductUploadQueue)
-        self.threads.append(product_upload_queue)
-
-        product_delete_queue = threading.Thread(target=ProductDeletionQueue)
-        self.threads.append(product_delete_queue)
-
-        product_image_queue = threading.Thread(target=ProductImageQueue)
-        self.threads.append(product_image_queue)
-
-        # Actually start the threads
-        ebay_queue.start()
-        product_upload_queue.start()
-        product_delete_queue.start()
-        product_image_queue.start()
-
-    def __finish_threads(self):
+    def __ebay_download_metadata(self):
         """
-            Blocks the parent thread until all children exit
+            Once we have gotten all of the items above,
+            we can get the ItemSpecifics and Description fields
         """
-        self.log.info('Waiting for all threads to finish')
-        for server_thread in self.threads:
-            server_thread.join()
+        # Disable downloading metadata due to the need to map attributes
+        # And I hit the time constraints to get this launched
+        # self.ebay.try_command('get_item_metadata')
+        pass
+
+    def __woo_upload_products(self):
+        """
+            Gets all of the active item ids and creates a
+            product on WooCommerce that matches its data
+        """
+        # TODO: Remove this after testing
+        item_ids = self.active_item_ids[0:20]
+
+        for item_id in item_ids:
+            self.woo.try_command('create_product', item_id)
+
+    def __woo_upload_metadata(self):
+        """
+            Once the products have been uploaded, we will
+            have the post ids for each product, which is
+            required to upload any images or other attributes
+        """
+        # TODO: Remove this after testing
+        item_ids = self.active_item_ids[0:20]
+
+        for item_id in item_ids:
+            self.woo.try_command('upload_images', item_id)
+            # Disable uploading attributes due to the need to map them
+            # And I hit the time constraints to get this launched
+            # self.woo.try_command('upload_attributes', item_id)
+
+    def __woo_delete_products(self):
+        """
+            Deletes any uploaded products that have either
+            been marked as Complete, or have reached their
+            end_date
+        """
+        for item_id in self.inactive_item_ids:
+            self.woo.try_command('delete_product', item_id)
 
     def start(self):
         """
-            Endpoint to kick off the server threads
-            and block until they are complete
+            Called to start everything.
 
-            Currently the only server thread is an
-            instance of `ProductUploadQueue`, which
-            itself will also spawn its own threads
-            (default is 1).
-
-            In the future, there will be another
-            thread that will handle getting products
-            from ebay and storing them in the database.
-
-            There may also be another thread to provide
-            a simple HTTP REST API
+            This will do the following in order:
+            1. Download products from ebay with GetSellerList
+            2. Download product ItemSpecifics with GetItem
+            3. Upload the downloaded products to WooCommerce
+            4. Upload the downloaded product metadata to WooCommerce
+            5. Delete any products that ended on ebay from WooCommerce
         """
-        self.__start_threads()
-        self.__finish_threads()
+        self.__ebay_download_products()
+        self.__ebay_download_metadata()
+        self.__woo_upload_products()
+        self.__woo_upload_metadata()
+        self.__woo_delete_products()
 
 if __name__ == '__main__':
-    Server()
+    Server().start()
