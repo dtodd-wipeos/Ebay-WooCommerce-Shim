287f16e7eb64fa134d1f4e0b98a18e16b48e08a6
David Todd <dtodd@oceantech.com>
2020-02-21 13:39:50 -0600

    Fix setting product category

--- bin/shim/woo.py
+++ bin/shim/woo.py
@@ -313,7 +313,7 @@ class WooCommerceShim(Database):
         }
 
         # Add the category id
-        category_id = self.get_mapped_category_id(item_id)
+        category_id = self.get_mapped_category_id(data.get('category_id', 0))
         if category_id is not None:
             upload_data['categories'] = [{ 'id': category_id }]
 
